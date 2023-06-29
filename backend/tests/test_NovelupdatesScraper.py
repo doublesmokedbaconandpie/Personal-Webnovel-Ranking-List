d2e10600adc22e39a396afbd8f6b06e588e8714b
Kevin Nguyen <kevinhnguyen@ucsb.edu>
2023-06-28 18:39:45 -0700

    one assert_equal per function

--- backend/tests/test_NovelupdatesScraper.py
+++ backend/tests/test_NovelupdatesScraper.py
@@ -2,14 +2,21 @@ import unittest
 from src.NovelupdatesScraper import NovelupdatesScraper
 
 class TestDefault(unittest.TestCase):
-    def test_default(self):
-        test = NovelupdatesScraper()        
-        self.assertEqual(test.tags, [])
-        self.assertEqual(test.genre, [])
-        self.assertEqual(test.title, "")
-        self.assertEqual(test.country, "")
-        self.assertEqual(test.novel_type, "")
-    
+    def setUp(self):
+        self.test = NovelupdatesScraper()       
+         
+    def test_tags(self):
+        self.assertEqual(self.test.tags, [])
+    def test_genre(self):
+        self.assertEqual(self.test.genre, [])
+    def test_title(self):
+        self.assertEqual(self.test.title, "")
+    def test_country(self):        
+        self.assertEqual(self.test.country, "")
+    def test_novel_type(self):        
+        self.assertEqual(self.test.novel_type, "")
+        
+class TestInvalidUrlHtml(unittest.TestCase):
     def test_invalid_url(self):
         test = NovelupdatesScraper(url = "akjsdhfjkahsdf")
         url_success = test.scrape_from_url()
@@ -30,70 +37,78 @@ class TestDefault(unittest.TestCase):
         file_success = test.import_html("tests/html_files/invalidhtml.testhtml")
         self.assertEqual(file_success, False)
 
-class TestScrapeNovelupdates(unittest.TestCase):
-    def test_otonari_file_vs_otonari_web(self):
-        test = NovelupdatesScraper()
-        import_success = test.import_html("tests/html_files/otonari.testhtml")
-        file_tags = test.tags
-        file_genre = test.genre
-        file_title = test.title
-        file_country = test.country
-        file_novel_type = test.novel_type
+class TestOtonari(unittest.TestCase):
+    def setUp(self) -> None:
+        self.file = NovelupdatesScraper()
+        self.import_success = self.file.import_html("tests/html_files/otonari.testhtml")
         
-        test.url = "https://www.novelupdates.com/series/otonari-no-tenshi-sama-ni-itsu-no-ma-ni-ka-dame-ningen-ni-sareteita-ken-ln/"
-        scrape_success = test.scrape_from_url()
-        web_tags = test.tags
-        web_genre = test.genre
-        web_title = test.title
-        web_country = test.country
-        web_novel_type = test.novel_type
+        self.url = NovelupdatesScraper()
+        self.url.url = "https://www.novelupdates.com/series/otonari-no-tenshi-sama-ni-itsu-no-ma-ni-ka-dame-ningen-ni-sareteita-ken-ln/"
+        self.scrape_success = self.url.scrape_from_url()
         
-        self.assertEqual(import_success, True)
-        self.assertEqual(scrape_success, True)
-        self.assertEqual(file_tags, web_tags)
-        self.assertEqual(file_genre, web_genre)
-        self.assertEqual(file_title, web_title)
-        self.assertEqual(file_country, web_country)
-        self.assertEqual(file_novel_type, web_novel_type)
-    
-    def test_otonari_file(self):
+    def test_tags(self):
         result_tags = ['Adapted to Anime', 'Adapted to Manga', 'Adapted to Visual Novel', 'Award-winning Work', 'Beautiful Female Lead', 'Calm Protagonist', 'Cold Love Interests', 'Cooking', 'Couple Growth', 'Criminals', 'Cute Story', 'Dense Protagonist', 'Handsome Male Lead', 'Heartwarming', 'Living Alone', 'Male Protagonist', 'Mature Protagonist', 'Modern Day', 'Popular Love Interests', 'R-15', 'Slow Romance']
+        self.assertEqual(self.file.tags, result_tags)
+    def test_genre(self):
         result_genre = ['Comedy', 'Romance', 'School Life', 'Slice of Life']
-        test = NovelupdatesScraper()
-        import_success = test.import_html("tests/html_files/otonari.testhtml")
-        
-        self.assertEqual(import_success, True)
-        self.assertEqual(test.tags, result_tags)
-        self.assertEqual(test.genre, result_genre)
-        self.assertEqual(test.title, "Otonari no Tenshi-sama ni Itsu no Ma ni ka Dame Ningen ni Sareteita Ken (LN)")
-        self.assertEqual(test.country, "JP")
-        self.assertEqual(test.novel_type, "Light Novel")
+        self.assertEqual(self.file.genre, result_genre)
+    def test_title(self):
+        self.assertEqual(self.file.title, "Otonari no Tenshi-sama ni Itsu no Ma ni ka Dame Ningen ni Sareteita Ken (LN)")
+    def test_country(self):        
+        self.assertEqual(self.file.country, "JP")
+    def test_novel_type(self):        
+        self.assertEqual(self.file.novel_type, "Light Novel")
     
-    def test_lotm_file(self):
+    def test_import_success(self):
+        self.assertEqual(self.import_success, True)
+    def test_scrape_success(self):
+        self.assertEqual(self.scrape_success, True)
+    def test_tags_web_vs_html(self):
+        self.assertEqual(self.file.tags, self.url.tags)
+    def test_genre_web_vs_html(self):
+        self.assertEqual(self.file.genre, self.url.genre)
+    def test_title_web_vs_html(self):
+        self.assertEqual(self.file.title, self.url.title)
+    def test_country_web_vs_html(self):        
+        self.assertEqual(self.file.country, self.url.country)
+    def test_novel_type_vs(self):        
+        self.assertEqual(self.file.novel_type, self.url.novel_type)
+
+class TestLotm(unittest.TestCase):
+    def setUp(self) -> None:
+        self.file = NovelupdatesScraper()
+        self.import_success = self.file.import_html("tests/html_files/lotm.testhtml")
+        
+    def test_tags(self):
         result_tags = ['Ability Steal', 'Absent Parents', 'Acting', 'Adapted to Drama CD', 'Adapted to Game', 'Adapted to Manhua', 'Alchemy', 'Alternate World', 'Aristocracy', 'Artifacts', 'Assassins', 'Award-winning Work', 'Calm Protagonist', 'Caring Protagonist', 'Cautious Protagonist', 'Character Growth', 'Clever Protagonist', 'Comedic Undertone', 'Cooking', 'Cunning Protagonist', 'Curses', 'Dark', 'Death', 'Death of Loved Ones', 'Depictions of Cruelty', 'Destiny', 'Detectives', 'Determined Protagonist', 'Dolls/Puppets', 'Dragons', 'Eidetic Memory', 'Elves', 'Evil Gods', 'Evil Organizations', 'Evil Religions', 'Familial Love', 'Family', 'Fantasy Creatures', 'Fantasy World', 'Fast Learner', 'Firearms', 'Ghosts', 'God Protagonist', 'Goddesses', 'Godly Powers', 'Gods', 'Hard-Working Protagonist', 'Harsh Training', 'Hidden Abilities', 'Hiding True Abilities', 'Hiding True Identity', 'Hunters', 'Immortals', 'Industrialization', 'Kingdoms', 'Lost Civilizations', 'Lucky Protagonist', 'Magic', 'Male Protagonist', 'Manipulative Characters', 'Mature Protagonist', 'Mind Break', 'Mind Control', 'Misunderstandings', 'Money Grubber', 'Monsters', 'Multiple Identities', 'Multiple POV', 'Multiple Realms', 'Multiple Reincarnated Individuals', 'Murders', 'Mutated Creatures', 'Mysterious Past', 'Mystery Solving', 'Mythical Beasts', 'Mythology', 'Nightmares', 'Nobles', 'Parasites', 'Past Plays a Big Role', 'Pets', 'Philosophical', 'Pirates', 'Police', 'Polite Protagonist', 'Proactive Protagonist', 'Protagonist with Multiple Bodies', 'Puppeteers', 'Religions', 'Resurrection', 'Righteous Protagonist', 'Saints', 'Schemes And Conspiracies', 'Sealed Power', 'Secret Identity', 'Secret Organizations', 'Secrets', 'Sentient Objects', 'Shapeshifters', 'Skill Assimilation', 'Slow Growth at Start', 'Souls', 'Special Abilities', 'Spirits', 'Strategic Battles', 'Thriller', 'Time Skip', 'Transformation Ability', 'Transmigration', 'Transplanted Memories', 'Trickster', 'Vampires', 'Wars', 'Weak to Strong', 'Wealthy Characters', 'Werebeasts', 'Zombies']
+        self.assertEqual(self.file.tags, result_tags)
+    def test_genre(self):
         result_genre = ['Comedy', 'Horror', 'Mature', 'Mystery', 'Psychological', 'Supernatural']
-        test = NovelupdatesScraper()
-        import_success = test.import_html("tests/html_files/lotm.testhtml")
+        self.assertEqual(self.file.genre, result_genre)
+    def test_title(self):
+        self.assertEqual(self.file.title, "Lord of the Mysteries")
+    def test_country(self):        
+        self.assertEqual(self.file.country, "CN")
+    def test_novel_type(self):        
+        self.assertEqual(self.file.novel_type, "Web Novel")
+
+class TestYoukoso(unittest.TestCase):
+    def setUp(self) -> None:
+        self.file = NovelupdatesScraper()
+        self.import_success = self.file.import_html("tests/html_files/youkoso.testhtml")
         
-        self.assertEqual(import_success, True)
-        self.assertEqual(test.tags, result_tags)
-        self.assertEqual(test.genre, result_genre)
-        self.assertEqual(test.title, "Lord of the Mysteries")
-        self.assertEqual(test.country, "CN")
-        self.assertEqual(test.novel_type, "Web Novel")
-        
-    def test_youkoso_file(self):
+    def test_tags(self):
         result_tags = ['Academy', 'Adapted to Anime', 'Adapted to Manga', 'Anti-social Protagonist', 'Antihero Protagonist', 'Apathetic Protagonist', 'Arrogant Characters', 'Award-winning Work', 'Awkward Protagonist', 'Beautiful Female Lead', 'Blackmail', 'Calm Protagonist', 'Cautious Protagonist', 'Character Growth', 'Clever Protagonist', 'Cold Protagonist', 'Cruel Characters', 'Cunning Protagonist', 'Different Social Status', 'Distrustful Protagonist', 'Fearless Protagonist', 'Genius Protagonist', 'Handsome Male Lead', 'Hiding True Abilities', 'Hiding True Identity', 'Love Interest Falls in Love First', 'Male Protagonist', 'Manipulative Characters', 'Mature Protagonist', 'Modern Day', 'Multiple POV', 'Mysterious Past', 'Overpowered Protagonist', 'Past Plays a Big Role', 'Philosophical', 'Proactive Protagonist', 'Protagonist Strong from the Start', 'Romantic Subplot', 'Ruthless Protagonist', 'Schemes And Conspiracies', 'Secretive Protagonist', 'Slow Romance', 'Stoic Characters', 'Strategic Battles', 'Strategist', 'Strength-based Social Hierarchy', 'Teamwork', 'Tragic Past', 'Underestimated Protagonist', 'Unreliable Narrator']
+        self.assertEqual(self.file.tags, result_tags)
+    def test_genre(self):
         result_genre = ['Drama', 'Psychological', 'School Life', 'Shounen', 'Slice of Life']
-        test = NovelupdatesScraper()
-        import_success = test.import_html("tests/html_files/youkoso.testhtml")
-        
-        self.assertEqual(import_success, True)
-        self.assertEqual(test.tags, result_tags)
-        self.assertEqual(test.genre, result_genre)
-        self.assertEqual(test.title, "Youkoso Jitsuryoku Shijou Shugi no Kyoushitsu e")
-        self.assertEqual(test.country, "JP")
-        self.assertEqual(test.novel_type, "Light Novel")
+        self.assertEqual(self.file.genre, result_genre)
+    def test_title(self):
+        self.assertEqual(self.file.title, "Youkoso Jitsuryoku Shijou Shugi no Kyoushitsu e")
+    def test_country(self):        
+        self.assertEqual(self.file.country, "JP")
+    def test_novel_type(self):        
+        self.assertEqual(self.file.novel_type, "Light Novel")   
 
 if __name__ == "__main__":
     unittest.main()
