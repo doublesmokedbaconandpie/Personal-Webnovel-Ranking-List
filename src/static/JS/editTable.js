54cd722854c0142ad74bd18639cf7e71fd32dfd3
Kevin Nguyen <kevinhnguyen@ucsb.edu>
2023-06-30 15:51:03 -0700

    lose focus upon press enter while editing

--- src/static/JS/editTable.js
+++ src/static/JS/editTable.js
@@ -1,6 +1,7 @@
-
-var addRow = document.getElementById("addRow");
-addRow.addEventListener("click", addRowClick);
+document.getElementById("addRow")
+        .addEventListener("click", addRowClick);
+document.querySelectorAll('td')
+        .forEach(e => e.addEventListener('keydown', editCell));
 
 function addRowClick() {
     var table = document.getElementById("NovelTable");
@@ -9,18 +10,15 @@ function addRowClick() {
     for (let i = 0; i < table.rows[0].cells.length; i++) {
         if (i == 0) {
             createCell(row.insertCell(i), `${table.rows.length - 1}.`, 'col0', false);
-            continue;
-        }
+            continue;}
         if (i == 2) {
             createCell(row.insertCell(i), "", 'col2', false);
-            let div1 = row.cells[2].childNodes[0];
+            let div1 = row.cells[2].children[0];
             div1.setAttribute("contenteditable", true);
-            continue;
-        }
+            continue;}
         if (i == 8) {
             createCell(row.insertCell(i), "", 'col8', false);
-            continue;
-        }
+            continue;}
         createCell(row.insertCell(i), "", `col${i}`, true);
     }
 }    
@@ -31,9 +29,7 @@ function createCell(cell, text, class_name, content_editable) {
     div.appendChild(txt);
     div.setAttribute('class', "scrollable");
     
-    if (content_editable) {
-        cell.setAttribute("contenteditable", "true");
-    }
+    if (content_editable) {cell.setAttribute("contenteditable", "true");}
     cell.setAttribute("class", class_name)
     cell.appendChild(div);
 }
@@ -41,10 +37,17 @@ function createCell(cell, text, class_name, content_editable) {
 function checkLastRowEmpty(table){
     var last_row = table.rows[table.rows.length - 1];
     for (let i = 1; i < last_row.cells.length; i++) {
-        let inner_div = last_row.cells[i].childNodes[0];
+        let inner_div = last_row.cells[i].children[0];
         if (inner_div.innerHTML != "") {
             return false;
         }
     }
     return true;
+}
+
+function editCell(evt){
+    if (evt.key === "Enter") {
+        console.log("Enter pressed!");
+        $('div[contenteditable="true"]').trigger('focus').trigger('blur');
+    }
 }
